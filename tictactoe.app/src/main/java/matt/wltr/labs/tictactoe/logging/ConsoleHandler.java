c613082012350a5e4807cbea502f7727be1b7582
Matthias Walter <matt.wltr@gmail.com>
2020-05-05 11:09:21 +0200

    replaced ConsoleHandler with custom Handler that logs via System.out.print

--- tictactoe.app/src/main/java/matt/wltr/labs/tictactoe/logging/ConsoleHandler.java
+++ tictactoe.app/src/main/java/matt/wltr/labs/tictactoe/logging/ConsoleHandler.java
@@ -1,9 +1,31 @@
 package matt.wltr.labs.tictactoe.logging;
 
-public class ConsoleHandler extends java.util.logging.ConsoleHandler {
+import java.text.MessageFormat;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
+public class ConsoleHandler extends Handler {
+
+    private static final Formatter FORMATTER = new SimpleFormatter();
 
     public ConsoleHandler() {
         super();
-        setOutputStream(System.out);
+    }
+
+    @Override
+    public void publish(LogRecord record) {
+        System.out.print(FORMATTER.format(record));
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public void close() throws SecurityException {
+
     }
 }
